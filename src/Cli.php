1895014c2e803e9bab5275a68a128e9a0e6ea590
Denis Skvortsov <skvord89@gmail.com>
2020-06-30 21:14:26 +0300

    Add logic for plain json-files

--- src/Cli.php
+++ src/Cli.php
@@ -11,10 +11,43 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: pretty]
 DOC;
-    Docopt::handle($doc);
+    $args = Docopt::handle($doc);
+    $firstFile = $args['<firstFile>'];
+    $secondFile = $args['<secondFile>'];
+
+    $firstFileData = file_get_contents($firstFile);
+    $secondFileData = file_get_contents($secondFile);
+
+    $firstFileJSONData = json_decode($firstFileData, true);
+    $secondFileJSONData = json_decode($secondFileData, true);
+
+    $firstKeys = array_keys($firstFileJSONData);
+    $secondKeys = array_keys($secondFileJSONData);
+    $unitedKeys = array_values(array_unique(array_merge($firstKeys, $secondKeys)));
+    $result = [];
+
+    foreach ($unitedKeys as $key) {
+        if (!in_array($key, $firstKeys) && in_array($key, $secondKeys)) {
+            $result[] = "  + {$key}: {$secondFileJSONData[$key]}";
+        } elseif (in_array($key, $firstKeys) && !in_array($key, $secondKeys)) {
+            $result[] = "  - {$key}: {$firstFileJSONData[$key]}";
+        } else {
+            if ($firstFileJSONData[$key] !== $secondFileJSONData[$key]) {
+                $result[] = "  + {$key}: {$secondFileJSONData[$key]}";
+                $result[] = "  - {$key}: {$firstFileJSONData[$key]}";
+            } else {
+                $result[] = "    {$key}: {$secondFileJSONData[$key]}";
+            }
+        }
+
+    }
+    $stringResult = "{\n" . implode("\n", $result) . "\n}\n";
+    print_r($stringResult);
 }
